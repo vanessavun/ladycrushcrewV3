52e77d39b9648e1da61e6d822e019d0c0703be83
Vanessa Vun <vanessavun@gmail.com>
2022-09-19 18:26:45 -0700

    Added shop categories and category items then routed shop categories

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState, useEffect } from "react";
+import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
+
+export const CategoriesContext = createContext({
+    categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+    const [categoriesMap, setCategoriesMap] = useState({});
+
+    //run once to fetch categories for shop
+    useEffect(() => {
+        const fetchCategories = () => {
+            const getCategoriesMap = async() => {
+                const categoryMap = await getCategoriesAndDocuments();
+                //console.log(categoryMap);
+                setCategoriesMap(categoryMap);
+            }
+            getCategoriesMap();
+        }
+        fetchCategories();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const value = { categoriesMap };
+
+    return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+}
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { UserProvider } from './contexts/user.context';
-import { ProductsProvider } from './contexts/products.context';
 import { CartProvider } from './contexts/cart.context';
+import { CategoriesProvider } from './contexts/categories.context';
 
 import './index.scss';
 
@@ -14,11 +14,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
