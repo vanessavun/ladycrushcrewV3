64eb5b53008a4400c03ff0130c3f47501ad57452
Vanessa Vun <vanessavun@gmail.com>
2022-12-13 11:48:19 -0800

    Migrated categories map to redux and added memoization

---
+++ src/store/categories/category.selector.js
@@ -0,0 +1,18 @@
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoryMap = createSelector(
+  [selectCategories],
+  (categories) => categories.reduce((acc, category) => {
+    const { title, items } = category;
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {})
+);
+
