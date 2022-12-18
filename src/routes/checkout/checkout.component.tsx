1f261030efc9391e9ba7b8cdde8d1673565d3cbc
Vanessa Vun <vanessavun@gmail.com>
2022-12-17 17:44:18 -0800

    Finished the rest of the typescript conversion: authentication, categories preview, checkout, shop, home

---
+++ src/routes/checkout/checkout.component.tsx
@@ -0,0 +1,50 @@
+import { useSelector } from "react-redux";
+import {
+  selectCartItems,
+  selectCartTotal,
+} from "../../store/cart/cart.selector";
+
+import CheckoutItem from "../../components/checkout-item/checkout-item.componenent";
+import PaymentForm from "../../components/payment-form/payment-form.component";
+
+import {
+  CheckoutContainer,
+  CheckoutHeader,
+  HeaderBlock,
+  Total
+} from "./checkout.styles";
+
+const CheckOut = () => {
+  const cartItems = useSelector(selectCartItems);
+  const total = useSelector(selectCartTotal);
+
+  return (
+    <CheckoutContainer>
+      <h1>Check Out</h1>
+      <CheckoutHeader>
+        <HeaderBlock>
+          <span>Product</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Description</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Quantity</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Price</span>
+        </HeaderBlock>
+        <HeaderBlock>
+          <span>Remove</span>
+        </HeaderBlock>
+      </CheckoutHeader>
+      {cartItems.map((item) => (
+        <CheckoutItem key={item.id} cartItem={item} />
+      ))}
+      <Total>Total: ${total}</Total>
+      <PaymentForm />
+    </CheckoutContainer>
+  );
+};
+
+export default CheckOut;
--- src/routes/shop/shop.component.tsx
+++ src/routes/shop/shop.component.tsx
@@ -6,8 +6,6 @@ import CategoriesPreview from "../categories-preview/categories-preview.componen
 import Category from "../category/category.component";
 import { fetchCategoriesStart } from '../../store/categories/category.action';
 
-import "./shop.styles.scss";
-
 const Shop = () => {
   const dispatch = useDispatch();
 
