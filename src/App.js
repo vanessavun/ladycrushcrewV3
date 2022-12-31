33ebdc1c9c09b2cd257a8721ebff7df3b0137d61
Vanessa Vun <vanessavun@gmail.com>
2022-12-30 22:01:51 -0800

    Added lazy loading for better performance

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import { useEffect } from "react";
-import { Routes, Route } from 'react-router-dom';
+import { useEffect, lazy, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
 
 import { GlobalStyle } from "./global.styles";
-
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Shop from "./routes/shop/shop.component";
-import CheckOut from './routes/checkout/checkout.component';
-import About from "./routes/about/about.component";
-import Events from "./routes/events/events.component";
-import Volunteer from "./routes/volunteer/volunteer.component";
-import Contact from "./routes/contact/contact.component";
 import { checkUserSession } from "./store/user/user.action";
 import { useDispatch } from "react-redux";
+import Spinner from "./components/spinner/spinner.component";
+
+const Home = lazy(() => import("./routes/home/home.component"));
+const Navigation = lazy(() => import("./routes/navigation/navigation.component"));
+const Authentication = lazy(() => import("./routes/authentication/authentication.component"));
+const Shop = lazy(() => import("./routes/shop/shop.component"));
+const CheckOut = lazy(() => import("./routes/checkout/checkout.component"));
+const About = lazy(() => import("./routes/about/about.component"));
+const Events = lazy(() => import("./routes/events/events.component"));
+const Volunteer = lazy(() => import("./routes/volunteer/volunteer.component"));
+const Contact = lazy(() => import("./routes/contact/contact.component"));
 
 function App() {
   const dispatch = useDispatch();
@@ -21,24 +22,24 @@ function App() {
   useEffect(() => {
     dispatch(checkUserSession());
   }, [dispatch]);
-  
+
   return (
-    <>
-    <GlobalStyle />
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='events' element={<Events />} />
-        <Route path='volunteer' element={<Volunteer />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<CheckOut />} />
-        <Route path='contact' element={<Contact />} />
-      </Route>
-    </Routes>
-    </>
-  )
-};
+    <Suspense fallback={<Spinner />}>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="events" element={<Events />} />
+          <Route path="volunteer" element={<Volunteer />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<CheckOut />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </Suspense>
+  );
+}
 
 export default App;
