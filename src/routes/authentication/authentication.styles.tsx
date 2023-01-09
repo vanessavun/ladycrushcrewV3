2f4b61453102bd86539ee92337ed8eef86cc3f98
Vanessa Vun <vanessavun@gmail.com>
2023-01-08 20:42:09 -0800

    Edited authentication page for responsiveness

--- src/routes/authentication/authentication.styles.tsx
+++ src/routes/authentication/authentication.styles.tsx
@@ -2,7 +2,14 @@ import styled from "styled-components";
 
 export const AuthenticationContainer = styled.div`
     display: flex;
-    width: 900px;
+    width: 800px;
     justify-content: space-between;
     margin: 30px auto;
+
+    @media screen and (max-width: 850px) {
+        width: 100%;
+        flex-direction: column;
+        align-items: center;
+        gap: 50px;
+    }
 `;
